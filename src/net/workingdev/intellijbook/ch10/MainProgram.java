177f6e19e807f3fd768b5fe4fffd3f4aba19690f
Administrator <aqwertg@126.com>
2022-07-28 12:24:40 +0800

    Added "hello gist public" statement

--- src/net/workingdev/intellijbook/ch10/MainProgram.java
+++ src/net/workingdev/intellijbook/ch10/MainProgram.java
@@ -4,5 +4,7 @@ public class MainProgram {
     public static void main(String[] args){
         System.out.println("Hello Git");
         System.out.println("Hello world");
+        System.out.println("Hello GitHub");
+        System.out.println("Hello Gist public");
     }
 }
